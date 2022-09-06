406e3a1496ee056e2f7a197ce504d18527f61317
Dhoni <ramadhoni@keyreply.com>
2022-09-06 11:13:25 +0700

    make functionalities

--- src/App.js
+++ src/App.js
@@ -1,16 +1,125 @@
-import { Container, Row, Col, Stack, Form, Card, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { Container, Row, Col, Stack, Form, Card, Button, Modal, Image } from 'react-bootstrap';
+import axios from 'axios';
+
+
+function CardNasa({ item }) {
+  const defaultPlaceholder = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1";
+  const [summarized, setSummarized] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={item.url || defaultPlaceholder} />
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          {
+            summarized ?
+              <Card.Text>
+                {item.desc.slice(0,75)}...
+                <Button onClick={() => setSummarized(false)} variant="link">show more</Button>
+              </Card.Text>
+            :
+              <Card.Text>
+                {item.desc}
+                <Button onClick={() => setSummarized(true)} variant="link">show less</Button>
+              </Card.Text>
+          }
+          <Button variant="primary" onClick={() => setShowModal(true)}>See in details</Button>
+        </Card.Body>
+      </Card>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{item.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image style={{ width: '100%' }} rounded={true} src={item.url || defaultPlaceholder} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
 function App() {
+  const NASA_KEY = "DEMO_KEY";
+  const BASE_URL = "https://api.nasa.gov";
+  const [loading, setLoading] = useState(false);
+  const [gallery, setGallery] = useState([]);
+  const [query, setQuery] = useState('');
+
+  function handleAPOD(e) {
+    e.preventDefault();
+    const APOD = `${BASE_URL}/planetary/apod?api_key=${NASA_KEY}`;
+    setLoading(true);
+    axios.get(APOD)
+      .then(({ data }) => {
+        setLoading(false);
+        const desc = data.explanation;
+        const newGallery = {
+          title: data.title,
+          desc,
+          url: data.url,
+        }
+        setGallery([newGallery, ...gallery]);
+      })
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+
+    const LIBRARY = "https://images-api.nasa.gov";
+    const endpoint = "/search";
+    const queryParam = `?q=${query}`;
+    const URL = `${LIBRARY}${endpoint}${queryParam}`;
+
+    setLoading(true);
+    axios.get(URL)
+      .then(({ data }) => {
+        const collections = data.collection.items;
+        let randomIndex = -1;
+        let linkIndex = -1;
+        if (collections.length === 0) {
+          setLoading(false);
+          return;
+        }
+        while (linkIndex < 0) {
+          randomIndex = Math.floor(Math.random() * (collections.length - 1));
+          if (collections[randomIndex].links && collections[randomIndex].links.length > 0) {
+            linkIndex = Math.floor(Math.random() * (collections[randomIndex].links.length - 1))
+          }
+        }
+        setLoading(false);
+
+        const desc = collections[randomIndex].data[0].description;
+        const newGallery = {
+          title: collections[randomIndex].data[0].title,
+          desc,
+          url: collections[randomIndex].links[linkIndex].href,
+        }
+        setGallery([newGallery, ...gallery]);
+      })
+  }
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
   return (
     <Container>
       <Row className="mt-5">
         <Col sm={3}>
-          <Button>Picture of the day</Button>
+          <Button disabled={loading} onClick={handleAPOD}>Picture of the day</Button>
         </Col>
         <Col sm={9}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Search</Form.Label>
-              <Form.Control type="email" placeholder="Search here..." />
+              <Form.Control onChange={handleChange} placeholder="Search here..." />
               <Form.Text className="text-muted">
                 Find amazing pic in NASA library.
               </Form.Text>
@@ -18,25 +127,26 @@ function App() {
           </Form>
         </Col>
       </Row>
-      <Row>
-        <Col className="my-3"style={{ textAlign: 'center' }}>
-          <h1>
-            Loading...
-          </h1>
-        </Col>
-      </Row>
-      <Stack style={{ flexWrap: 'wrap' }} direction="horizontal" gap={3}>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">See in details</Button>
-          </Card.Body>
-        </Card>
+      {
+        loading ?
+          <Row>
+            <Col className="my-3"style={{ textAlign: 'center' }}>
+              <h1>
+                Loading...
+              </h1>
+            </Col>
+          </Row>
+        :
+          <></>
+      }
+      <Stack style={{ flexWrap: 'wrap', justifyContent: 'center' }} direction="horizontal" gap={4}>
+        {
+          gallery.map((item, index) => {
+            return (
+              <CardNasa key={index} item={item}></CardNasa>
+            )
+          })
+        }
       </Stack>
     </Container>
   );
